8d768c74788ff9b8d6278ce4dc07c7db1106a60e
fab1kkk <fabian.ponitka.shoplet@gmail.com>
2023-10-11 18:09:22 +0200

    100% tests coverage for test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,6 @@ import unittest
 import os
 
 from utils import *
-from configs import MODES
 
 ROOT_DIR = os.getcwd()
 TEST_DIR = ROOT_DIR + r"\tests\sample_files"
@@ -16,6 +15,12 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(len(get_files_from_dir(TEST_DIR)), total_files_in_test_dir)
         for file in files_in_test_dir:
             self.assertIn(file, get_files_from_dir(TEST_DIR))
+            
+    def test_get_files_from_not_existing_dir(self):
+        self.assertRaises(FileNotFoundError, lambda: get_files_from_dir('path/that/doesnt/exist'))
+        
+    def test_get_files_from_dir_invalid_input(self):
+        self.assertRaises(TypeError, lambda: get_files_from_dir(None))
         
     def test_get_file_extension_with_extension(self):
         docextension = get_file_extension('word.doc')
@@ -32,11 +37,18 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(noextension, None)
         
     def test_mode_allowed(self):
-        modes = [mode for mode in MODES]
-        for mode in modes:
-            self.assertEqual(True, mode_allowed(mode))
+        modeslist = ['list1', 'list2']
+        modeset = {'set1', 'set2'}
+        modestuple = ('tuple1', 'tupl2')
+        self.assertEqual(True, mode_allowed('list1', modeslist))
+        self.assertEqual(True, mode_allowed('set1', modeset))
+        self.assertEqual(True, mode_allowed('tuple1', modestuple))
+        
+        self.assertEqual(False, mode_allowed('fakemode', modeslist))
+        self.assertEqual(False, mode_allowed('fakemode', modeset))
+        self.assertEqual(False, mode_allowed('fakemode', modestuple))
 
-    def test_mode_not_allowed(self):
-        mode = 'thereisnosuchmode'
-        self.assertEqual(False, mode_allowed(mode))
+    def test_mode_allowed_invalid_input(self):
+        modes = ['mode1', 'mode2', 2, 3]
+        self.assertRaises(TypeError, lambda : mode_allowed(None, modes))
         
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import os
+from typing import Union
+
 from configs import MODES
 
-def get_file_extension(file : str) -> str | None:
+
+def get_file_extension(file: str) -> str | None:
     """
     Extracts and returns the file extension from a given file name or path.
 
@@ -25,18 +28,70 @@ def get_file_extension(file : str) -> str | None:
     """
     if not isinstance(file, str):
         raise TypeError(f"file must be str type, {type(file)} given")
-    
-    extension = file.split('.')[-1]
-    
+
+    extension = file.split(".")[-1]
     if extension == file:
         return None
     return extension
 
-def get_files_from_dir(path) -> list[str]:
-    return os.listdir(path)
 
-def mode_allowed(mode: str) -> bool:
+def get_files_from_dir(path: str) -> list[str]:
+    """
+    Get a list of files in the specified directory.
+
+    Parameters:
+    path (str): A string representing the directory path.
+
+    Returns:
+    list[str]: A list of file names in the directory.
+
+    Raises:
+    TypeError: If the 'path' parameter is not of type str.
+    FileNotFoundError: If the specified path is not found.
+
+    Example:
+    >>> get_files_from_dir("/path/to/directory")
+    ['file1.txt', 'file2.jpg']
+    """
+    if not isinstance(path, str):
+        raise TypeError(f"type must be str type, {type(path)} given")
+    try:
+        return os.listdir(path)
+    except FileNotFoundError as e:
+        raise e
+
+
+def mode_allowed(
+    mode: str, allowed_modes: Union[list[str], tuple[str], set[str]]
+) -> bool:
+    """
+    Check if the given mode is allowed based on an iterable of allowed modes.
+
+    Parameters:
+    mode (str): A string representing the mode to be checked.
+    allowed_modes (Union[list[str], tuple[str], set[str]]): 
+    An iterable (e.g., list, tuple, or set) of allowed modes.
+
+    Returns:
+    bool: True if the mode is allowed (found in the iterable of allowed modes),
+          False otherwise.
+
+    Raises:
+    TypeError: If the 'mode' parameter is not of type str.
+    TypeError: If the 'allowed_modes' parameter is not an iterable containing strings.
+
+    Example:
+    >>> allowed_modes = ["read", "write", "execute"]
+    >>> mode_allowed("read", allowed_modes)
+    True
+    >>> mode_allowed("modify", allowed_modes)
+    False
+    """
     if not isinstance(mode, str):
         raise TypeError(f"type must be str type, {type(mode)} given")
-    
-    return True if mode.lower() in MODES else False
+    if not isinstance(allowed_modes, (list, tuple, set)) or not all(
+        isinstance(item, str) for item in allowed_modes
+    ):
+        raise TypeError("allowed_modes must be an iterable containing strings")
+
+    return True if mode.lower() in allowed_modes else False
