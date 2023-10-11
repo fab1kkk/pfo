1a248f6e44425c054f17340f34364b7075ff3c51
fab1kkk <fabian.ponitka.shoplet@gmail.com>
2023-10-11 12:59:34 +0200

    refactored classes, added FileOrganizer, OrganizerIO

--- core.py
+++ core.py
@@ -1,50 +1,68 @@
-import utils
+from utils import type_allowed, get_files_from_dir, get_file_extension
 
-class PathModel:
-    def __init__(self):
-        self._path = None
+from configs import TYPES
+
+
+class FileOrganizer:
+    def __init__(self, type: str): 
+        self._type = type
+
+    @property
+    def type(self):
+        return self._type
+            
+    @type.setter
+    def type(self, newtype):
+        if not type_allowed(newtype):
+           raise Exception(f"Type must ba an allowed type.\nAllowed types:\n{TYPES}")
+        self._type = newtype.lower()
         
-    def set_path(self, path):
-        self._path = path
-        
-    def get_path(self):
-        return self._path
-    
-class FileView:
-    def get_path_from_user(self):
+    def organize(self):
+        pass
+
+
+class OrganizerIO:
+    """A utility class for handling file organization input and output.
+
+    This class provides methods for interacting with the user to obtain
+    file paths to be organized and for displaying information related to
+    selected paths.
+    """
+
+    @staticmethod
+    def get_path_from_user():
         return input("Enter a path to be organized: ")
-    
-    def display_path(self, path):
+
+    @staticmethod
+    def display_path(path):
         print(f"Selected path {path}")
-        
-        
+
+
 class FileDir:
     def __init__(self, path_model, view):
         self.path_model = path_model
         self.view = view
-    
+
     def ask_for_path(self):
         file_path = self.view.get_path_from_user()
         self.path_model.set_path(file_path)
-        
+
     def show_path(self):
         file_path = self.path_model.get_path()
         self.view.display_path(file_path)
-        
+
     def get_files(self) -> list | None:
         if not self.path_model.get_path():
             p = self.view.get_path_from_user()
             self.path_model.set_path(p)
-        files = utils.get_files_from_dir(self.path_model.get_path())
+        files = get_files_from_dir(self.path_model.get_path())
         return files
-        
+
     def get_extensions(self) -> list | None:
         extensions = []
         files_in_dir = self.get_files()
         for f in files_in_dir:
-            ext = utils.get_file_extension(f)
+            ext = get_file_extension(f)
             extensions.append(ext)
-        
+
         return extensions
-        
-        
--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import os
+from configs import TYPES
 
 def get_file_extension(file : str):
     return file.split('.')[-1]
 
 def get_files_from_dir(path) -> list:
     return os.listdir(path)
+
+def type_allowed(type):
+    if type:
+        return True if type.lower() in TYPES else False
+    return False
